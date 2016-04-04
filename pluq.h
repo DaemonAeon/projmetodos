00b1e6138eb6ee7efb5a1748509f442cdb7b50f8
Katherine Lozano <katherinelozm@gmail.com>
2016-04-04 09:57:15 -0600

    PLUQ2

--- pluq.h
+++ pluq.h
@@ -1,10 +1,22 @@
 //pluq.h
 #ifndef PLUQ_H
 #define PLUQ_H
+
 class pluq{
 public:
 	pluq();
+	pluq(int);
 	~pluq();
+	void calculate();
+private:
+	double** P;
+	double** A;
+	double** Q;
+	double** L;
+	double** U;
+	double** AInversa;
+	int n;
+	void lu();
 };
 #endif
 //pluq.h
